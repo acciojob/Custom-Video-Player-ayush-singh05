453f269e313ddfe4b28daafa624318986f6114fa
Ayush Singh <99062720+ayush-singh05@users.noreply.github.com>
2023-10-02 22:02:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,3 +6,23 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+document.addEventListener("DOMContentLoaded", function () {
+    // const video = document.getElementById("video");
+    const progressBar = document.getElementById("progress-bar");
+
+    progressBar.addEventListener("timeupdate", updateProgressBar);
+
+    function updateProgressBar() {
+        const currentTime = video.currentTime;
+        const duration = video.duration;
+        const progress = (currentTime / duration) * 100;
+        progressBar.style.width = progress + "%";
+    }
+});
+ skipButtons.dataset.-10.addEventListener("click", () => {
+        video.currentTime -= 10; // Adjust the skip duration as needed (e.g., skip back 10 seconds)
+    });
+
+    skipButtons.dataset.25.addEventListener("click", () => {
+        video.currentTime += 10; // Adjust the skip duration as needed (e.g., skip forward 10 seconds)
+    });
